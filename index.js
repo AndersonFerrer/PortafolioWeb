10bf6f366f6688587d81be67ec6f69ed2dfc4fc1
AndersonFerrer <ferrer.280104@gmail.com>
2023-03-10 22:15:02 -0500

    Contacto y Footer

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function focusLabel(elemId) {
     const elem = document.getElementById(elemId)
     elem.style.color = '#60A5FA'
-    
   }
 
   function blurLabel(elemId, elem) {
-
     if (elem.value !== ''){
         const color = document.getElementById(elemId);
         color.style.color = '#60A5FA'
