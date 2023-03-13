cd2ef22aab03fecbf2419ad0d97f3307ff7625a3
AndersonFerrer <ferrer.280104@gmail.com>
2023-03-12 23:51:30 -0500

    Final v1

--- index.js
+++ index.js
@@ -14,21 +14,41 @@ function blurLabel(elemId, elem) {
     
  }
 
-var visible = true;
+var visible = false;
 const headerNav = document.getElementById('header-nav');
-  const blackMenu = document.getElementById('black-menu');
+const blackMenu = document.getElementById('black-menu');
+const bar1 = document.getElementById('bars1');
+const bar2 = document.getElementById('bars2');
+const bar3 = document.getElementById('bars3');
 
 function mostrarMenu() {
   headerNav.style.right = '0px';
-  blackMenu.style.display = 'flex'; 
+  blackMenu.style.display = 'flex';
+  bar1.style.rotate = '45deg'; 
+  bar3.style.rotate = '-45deg';
+  bar2.style.opacity = '0';  
 }
 function ocultarMenu() {
-  headerNav.style.right = '-60%';
+  bar1.style.rotate = '0deg'; 
+  bar3.style.rotate = '0deg';
+  bar2.style.opacity = '1';  
+  headerNav.style.right = '-100%';
   blackMenu.style.display = 'none'; 
-  console.log('haaa')
+  visible = !visible;
 }
 
-document.getElementById('btn-menu').addEventListener('click', mostrarMenu);
+function btnMenu() {
+  if(visible == false) {
+    console.log(visible)
+    mostrarMenu();
+    visible = !visible
+  } else if (visible == true) {
+    console.log(visible)
+    ocultarMenu()
+    
+  }
+}
+
 document.getElementById('black-menu').addEventListener('click', ocultarMenu);
 document.getElementById('header-nav').addEventListener('click', ocultarMenu)
 
