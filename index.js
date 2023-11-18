03a877162d33e1bff059c974c41e786ef62f847a
AndersonFerrer <ferrer.280104@gmail.com>
2023-11-18 01:10:31 -0500

    updated cv & tecnologies

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 function focusLabel(elemId) {
-    const elem = document.getElementById(elemId)
-    elem.style.color = '#60A5FA'
-  }
+  const elem = document.getElementById(elemId)
+  elem.style.color = '#60A5FA'
+}
 
 function blurLabel(elemId, elem) {
 
   if (elem === email) {
 
     const color = document.getElementById(elemId);
-    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     let emailValid = expReg.test(elem.value);
-    console.log(elem.value , emailValid)
-    if(emailValid == true) {
+    console.log(elem.value, emailValid)
+    if (emailValid == true) {
       color.style.color = '#60A5FA'
     }
-     else {
+    else {
       color.style.color = 'var(--label-ligth)'
     }
 
   }
 
 
-  if (elem !== email && elem.value !== ''){
-      const color = document.getElementById(elemId);
-      color.style.color = '#60A5FA'
-   } 
-   else if (elem !== email){
-      const color = document.getElementById(elemId)
-      color.style.color = 'var(--label-ligth)'
-   }
-    
- }
+  if (elem !== email && elem.value !== '') {
+    const color = document.getElementById(elemId);
+    color.style.color = '#60A5FA'
+  }
+  else if (elem !== email) {
+    const color = document.getElementById(elemId)
+    color.style.color = 'var(--label-ligth)'
+  }
+
+}
 
 let visible = false;
 const headerNav = document.getElementById('header-nav');
@@ -41,32 +41,32 @@ const bar3 = document.getElementById('bars3');
 function mostrarMenu() {
   headerNav.style.right = '0px';
   blackMenu.style.display = 'flex';
-  bar1.style.rotate = '45deg'; 
+  bar1.style.rotate = '45deg';
   bar3.style.rotate = '-45deg';
-  bar2.style.opacity = '0';  
+  bar2.style.opacity = '0';
 }
 function ocultarMenu() {
-  bar1.style.rotate = '0deg'; 
+  bar1.style.rotate = '0deg';
   bar3.style.rotate = '0deg';
-  bar2.style.opacity = '1';  
+  bar2.style.opacity = '1';
   headerNav.style.right = '-100%';
-  blackMenu.style.display = 'none'; 
+  blackMenu.style.display = 'none';
   visible = !visible;
 }
 
 function btnMenu() {
-  if(visible == false) {
+  if (visible == false) {
     mostrarMenu();
     visible = !visible
   } else if (visible == true) {
     ocultarMenu()
-    
+
   }
 }
 let darkModeIs = false;
 function darkMode() {
   console.log('clickkk')
-  if(darkModeIs === true) {
+  if (darkModeIs === true) {
     darkModeIs = !darkModeIs
     visible = !visible
     document.documentElement.style.setProperty('--ligth-mode', 'white')
@@ -103,4 +103,3 @@ window.addEventListener('resize', esconder)
 function esconder() {
   blackMenu.style.display = 'none'
 }
-
